9afe382b8423d0a7f405e3b04b8cc2d807a11dec
Eastroots92 <eastroots92@gmail.com>
2018-03-20 15:32:09 +0900

    ICON 추가 & CustomFont 추가 Activity 경로 수정, Color 추가

---
+++ app/src/main/java/openull/com/dailyqt/ui/splash/Splash.kt
@@ -0,0 +1,13 @@
+package openull.com.dailyqt.ui.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import openull.com.dailyqt.R
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
